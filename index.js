32b6e9dfa8dfbfbb763e837e261c6f6a87dc00d6
Tarun Dhiman <tarun.dhiman@eisenvault.com>
2021-08-04 12:28:26 +0530

    changes

--- index.js
+++ index.js
@@ -18,15 +18,28 @@ let count = 0
 
 io.on('connection', (socket) => {
 
+    // socket.emit sends the message to the current connected user
     socket.emit('connected', 'Welcome to our app !')
+
+    // socket.broadcast.emit sends the message to all connected sockets except current user
+    socket.broadcast.emit('connected','A new user joined the chat')
 
     socket.on('countChanged', () => {
         count ++
+        // io.emit send message to all connected users
         io.emit('countUpdated', count)
     })
 
     socket.on('messageSend', (message) => {
         io.emit('readMessage', message)
+    })
+
+    socket.on('geolocationSend', (location) => {
+        socket.broadcast.emit('readGeolocation', location)
+    }) 
+
+    socket.on('disconnect', () => {
+        io.emit('readMessage','A user left the chat !')
     })
 })
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,31 @@
 const socket = io()
 
+// socket connection message
 socket.on('connected', (message) => {
     console.log(message)
 })
 
+// get the updated count
 socket.on('countUpdated', (count) => {
     console.log('count updated', count)
 })
 
+// read the message
 socket.on('readMessage', (message) => {
     console.log(message)
 })
 
+//read the geolocation
+socket.on('readGeolocation', (location) => {
+    console.log(location)
+})
+
+// increment a count
 document.querySelector('#increment').addEventListener('click', () => {
     socket.emit('countChanged')
 })
 
+// send a message
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault()
     
@@ -26,4 +36,20 @@ document.querySelector('form').addEventListener('submit', (e) => {
     }
 
     socket.emit('messageSend', message)
+})
+
+document.querySelector('#location').addEventListener('click', (e) => {
+    if(!navigator?.geolocation){
+        return alert('your browser does not supports location sharing !')
+    }
+
+    navigator.geolocation.getCurrentPosition((position)=> {
+
+        let cords = {
+            latitude:position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+
+        socket.emit('geolocationSend', cords)
+    })
 })
